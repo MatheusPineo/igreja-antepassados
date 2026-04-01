3f3a23533da37a41e6e55e038af6f969cc39c6fa
Matheus Pineo <matheuskrx@gmail.com>
2026-04-01 23:33:29 +0100

    fix: final websocket and deploy url configuration

--- rxconfig.py
+++ rxconfig.py
@@ -1,11 +1,11 @@
 import reflex as rx
 import os
 
-# Pega a URL do banco de dados
 db_url = os.getenv("DATABASE_URL", "sqlite:///reflex.db")
 
 config = rx.Config(
     app_name="projeto_messianica",
     db_url=db_url,
     api_url="https://igreja-antepassados.onrender.com",
+    deploy_url="https://igreja-antepassados.onrender.com",
 )
